a862d48ebdf12a55b3ecd573a8eccc5049849b0c
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-15 03:33:33 -0700

    0x00-hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * return: if no errors return 0 else return value
+ */
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return(0)
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main- entry point
+ *
+ * Return:if no errors return 0 else return value
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
