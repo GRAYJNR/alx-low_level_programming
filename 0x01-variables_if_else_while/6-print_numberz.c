19142051f051cda5dbba69b2b4bba238edf651cd
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-17 14:36:09 -0700

    0x01 variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits in base 10
+ * Return: Always 0
+ */
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+		putchar((num % 10) + '0');
+	putchar ('\n');
+	return (0);
+}
