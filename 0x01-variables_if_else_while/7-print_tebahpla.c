c165cd2b2e31917e2a9c81355a356337428d2c18
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-19 00:17:41 -0700

    0x01 variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - print the alphabets in reverse
+ * Return: Always 0
  */
 
 int main(void)
@@ -13,6 +11,5 @@ int main(void)
 
 	for (le = 'z'; le >= 'a'; le--)
 		putchar(le);
-		putchar('\n');
 	return (0);
 }
