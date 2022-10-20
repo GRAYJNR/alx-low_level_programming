47ed4e4e2fd6e5c56d979aff2d6411ee8ebfa3e6
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-20 07:59:26 -0700

    0x02-functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two minutes
+ * @a: the first number
+ * @b: the second number
+ * Return: the dirsction of both
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
