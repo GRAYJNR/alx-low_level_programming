2c5bae75fdfa63479235896678f64ad0a789da9a
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-22 11:35:00 -0700

    0x02 functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,7 +12,7 @@ int print_sign(int n)
 {
 	if (n>0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n==0)
@@ -20,7 +20,7 @@ int print_sign(int n)
 		_putchar('0');
 		return (0);
 	}
-	else (n < 0)
+	else (n<0)
 	{
 		_putchar('_');
 		return (-1);
