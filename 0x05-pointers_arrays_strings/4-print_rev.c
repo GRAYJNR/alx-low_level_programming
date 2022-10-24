9efbeee757fd39dab51ae4c2674396fb9dbd918e
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-24 04:21:44 -0700

    0x05 pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,10 @@
 void print_rev(char *s)
 {
 	int i = 0
-		while (s[i] != '\0')
+		
+	while (s[i] != '\0')
 			i++;
 	for (i = -1; i >= 0; i--)
 		_putchar(s[i]);
-	_putchar('\n')
+	_putchar('\n');
 }
