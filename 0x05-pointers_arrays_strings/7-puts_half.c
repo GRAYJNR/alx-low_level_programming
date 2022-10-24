a8071f63075e00c79b43d0810aa8d681e8bccffe
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-24 05:14:29 -0700

    0x05 pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,19 +5,20 @@
  * @str: input
  */
 
-void puts_half (char *str)
+void puts_half(char *str)
 {
 	int len = 0;
+
 	while (*str != '\0')
 	{
 		len++;
 		str++;
 	}
-	str -= (len /2);
+	str -= (len / 2);
 	while (*str != '\0')
 	{
 		_putchar(*str);
 	str++;
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
