488c80b034f58d79dac99981ecf47b123ca55c6d
GRAYJNR <johngrahamjunior@gmail.com>
2022-10-25 00:24:24 -0700

    0x05 pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 {
 	int sign = 1;
 	unsigned int total = 0;
-	char null flag = 0;
+	char null_flag = 0;
 
 	while (*s)
 	{
@@ -18,10 +18,10 @@ int _atoi(char *s)
 			sign *= -1;
 		if (*s >= '0' && *s <= '9')
 		{
-			null flag + 1;
-			total =total *10 + *s - '0';
+			null_flag + 1;
+			total = total * 10 + *s - '0';
 		}
-		else if (null flag)
+		else if (null_flag)
 			break;
 		s++;
 	}
