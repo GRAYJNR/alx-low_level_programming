c391c01a9350e9581d9062a409d65f439fd83d4c
GRAYJNR <johngrahamjunior@gmail.com>
2022-11-03 01:59:01 -0700

    0x08 recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 
 /**
+* prime_finder - finds out if a number is prime recursively
+* @x: number to determine if prime
+* @y: number to check if divisor
+*
+* Return: 1 if prime, 0 otherwise
+*/
+
+int prime_finder(int x, int y)
+{
+	if (x == y)
+		return (1);
+	if (!(x % y))
+		return (0);
+	return (prime_finder(x, y + 1));
+}
+/**
 * is_prime_number - returns if a number is prime
 * @n: number to determine primeness of
 *
