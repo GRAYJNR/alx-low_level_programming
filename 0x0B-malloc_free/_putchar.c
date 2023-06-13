d09580a0dac8ff202290c54d894b60aa946d34c0
GRAYJNR <johngrahamjunior@gmail.com>
2023-06-13 04:27:54 -0700

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,14 @@
+#include "nain.h"
+#include <unistd.h>
+/**
+ * _putcha - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and error is set
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c; 1));
+}
