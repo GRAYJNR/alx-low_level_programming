cd5ddbcbf40757e816405548d5ef3f00feb10589
GRAYJNR <johngrahamjunior@gmail.com>
2023-06-14 07:25:10 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@
 int **alloc_grid(int width, int height)
 {
 	int **gray;
-	ont i, j;
+	int i, j;
 	if (height <= 0 || width <= 0)
 		return (NULL);
 
@@ -37,5 +37,5 @@ int **alloc_grid(int width, int height)
 			gray[i][j] = 0;
 		}
 	}
-	return (gray)
+	return (gray);
 }
