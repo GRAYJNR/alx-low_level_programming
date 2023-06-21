8c3d8ae8248354524ec4119bbc732d680d024447
GRAYJNR <johngrahamjunior@gmail.com>
2023-06-21 14:17:06 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,12 +6,12 @@
 * @size: this represents size of array (num of elements)
 * @cmp: this is the comparison function to determine if element found
 *
-* Return: this is an integer that represents index of element 
+* Return: this is an integer that represents index of element
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int j = 0;
-	
+
 	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
